ba6071ff3149fedd26cc898593f92084e2ef864d
zain.aaban <zain.aaban@WH-S2113-S-10.ccac.org>
2018-12-03 15:15:17 -0500

    EDarsow's code

---
+++ javaoo_netbeans/src/fightclubrefactoring/Person.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fightclubrefactoring;
+
+/**
+ *
+ * @author zain.aaban
+ */
+public class Person {
+    
+    public String fname;
+    public String lname;
+    public String phoneNum;
+    
+}
